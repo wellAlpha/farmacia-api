945c0aeb55f499a6cb0cfc9545e81ec9f6ad3cc2
Wellington Monteiro <wellington.m.alves@hotmail.com>
2023-11-21 00:12:21 -0300

    draft: remover depois.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,9 +2,9 @@ output "database_instance_ip" {
   value = google_sql_database_instance.farmacia.ip_address
 }
 
-output "database_instance_port" {
-  value = google_sql_database_instance.farmacia.port
-}
+# output "database_instance_port" {
+#   value = google_sql_database_instance.farmacia.port
+# }
 
 # output "database_name" {
 #   value = google_sql_database_instance.farmacia.database
