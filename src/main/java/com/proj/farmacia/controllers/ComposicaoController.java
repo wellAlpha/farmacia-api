aaa428680dd2b68bcf80280e65ee1b9b681f9a0f
Wellington Monteiro <wellington.m.alves@hotmail.com>
2023-12-08 13:48:26 -0300

    chore: adicionando cors.

--- src/main/java/com/proj/farmacia/controllers/ComposicaoController.java
+++ src/main/java/com/proj/farmacia/controllers/ComposicaoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.proj.farmacia.entities.Composicao;
 import com.proj.farmacia.services.ComposicaoService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController()
 @RequestMapping("/api/composicoes")
 public class ComposicaoController {
